94dce62445fa6aef6a32373c9562496aff1e6a35
Vishal Gupta <vishal.mca20.du@gmail.com>
2022-11-25 12:40:01 +0530

    Time: 2 ms (69.78%), Space: 6.2 MB (8.19%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
-    int recur(int n){
-        if(n<=0) return 0;
-        if(n==1) return 1;
-        else return recur(n-1)+recur(n-2);
+    int recur(int n,vector<int> &dp){
+        if(n<=1) return n;
+        if(dp[n] != -1) return dp[n];
+        else return dp[n] = recur(n-1,dp)+recur(n-2,dp);
     }
     int fib(int n) {
-        return recur(n);
+        vector<int> dp(n+1,-1);
+        return recur(n,dp);
+        
     }
 };
