9822ead4e328915c42e49248f5e563c6a824cb9f
Vishal Gupta <vishal.mca20.du@gmail.com>
2023-01-29 00:18:48 +0530

    Time: 3 ms (46.03%), Space: 6 MB (44.37%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int numTrees(int n) {
-        if(n<=1) return 1;
+        // if(n<=1) return 1;
+        int dp[n+1];
+        dp[0] = dp[1] = 1;
         
         int res = 0;
         
-        for(int i=0; i<n; i++){
-            res += numTrees(i) * numTrees(n-i-1);
+        for(int i=2; i<=n; i++){
+            dp[i] = 0;
+            for(int j=0; j<i; j++){
+                dp[i] += dp[j] * dp[i-j-1];
+            }
+            // res += numTrees(i) * numTrees(n-i-1);
         }
-        return res;
+        return dp[n];
         
     }
 };
