79c57f307270a9b48c5bca9afb9551fbdedcebd6
Vishal Gupta <vishal.mca20.du@gmail.com>
2023-04-08 23:56:35 +0530

    Time: 38 ms (75.27%), Space: 14.6 MB (17.48%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,13 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+# import copy
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        l2 = copy.deepcopy(node)
+        return l2
+        
